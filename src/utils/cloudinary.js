0529efe19fae0840933bf961216e0f41a4fdf414
Dev Kinariwala <143329543+devkinariwala@users.noreply.github.com>
2025-06-21 10:39:20 +0530

    file handeling

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+(async function () {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+});
+const uploadOnCloudinary = async (localFIlePath) => {
+  try {
+    if (!localFIlePath) return null;
+    const response = await cloudinary.uploader.upload(localFIlePath, {
+      resource_type: "auto",
+    });
+    console.log("File Uploaded on Cloudinary Successfully!", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFIlePath); //local system me se file ko nikal dega
+  }
+};
+
+export { uploadOnCloudinary };
